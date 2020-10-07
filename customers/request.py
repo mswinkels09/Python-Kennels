0471c4075e5eb33858cac2df199733457e2e8043
mswinkels09 <mswinkels09@gmail.com>
2020-10-07 15:34:19 -0500

    added get all resources by id, created post functions for all resources

---
+++ customers/request.py
@@ -0,0 +1,38 @@
+CUSTOMERS = [
+    {
+      "id": 1,
+      "name": "Hannah Hall",
+      "address": "7002 Chestnut Ct"
+    },
+    {
+      "id": 2,
+      "name": "Jeniffer Mills",
+      "address": "1000 Lake Ct"
+    },
+    {
+      "email": "1@1.com",
+      "password": "1",
+      "name": "mik swink",
+      "id": 3
+    }
+]
+
+
+def get_all_customers():
+    return CUSTOMERS
+
+# Function with a single parameter
+def get_single_customer(id):
+    # Variable to hold the found customer, if it exists
+    requested_customer = None
+
+    # Iterate the CUSTOMERS list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for customer in CUSTOMERS:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if customer["id"] == id:
+            requested_customer = customer
+
+    return requested_customer
+
--- request_handler.py
+++ request_handler.py
@@ -1,9 +1,31 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals
+from animals import get_all_animals, get_single_animal, create_animal
+from customers import get_all_customers, get_single_customer
+from employees import get_all_employees, get_single_employee, create_employee
+from locations import get_all_locations, get_single_location, create_location
+import json
 
 
 # Here's a class. It inherits from another class.
 class HandleRequests(BaseHTTPRequestHandler):
+    def parse_url(self, path):
+        # Just like splitting a string in JavaScript. If the
+        # path is "/animals/1", the resulting list will
+        # have "" at index 0, "animals" at index 1, and "1"
+        # at index 2.
+        path_params = path.split("/") #{ "", "animals"}
+        resource = path_params[1]
+        id = None
+
+        # Try to get the item at index 2
+        try:
+            id = int(path_params[2]) # if there is no index 2 (id) then it will throw an error, which is why we put try infront and the excepts
+        except IndexError:
+            pass  # No route parameter exists: /animals
+        except ValueError:
+            pass  # Request had trailing slash: /animals/
+
+        return (resource, id)  # This is a tuple
 
     # Here's a class function
     def _set_headers(self, status):
@@ -15,31 +37,76 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Here's a method on the class that overrides the parent's method.
     # It handles any GET request.
     def do_GET(self):
-        # Set the response code to 'Ok'
         self._set_headers(200)
+        response = {}  # Default response
 
-        # Your new console.log() that outputs to the terminal
-        print(self.path)
+        # Parse the URL and capture the tuple that is returned
+        (resource, id) = self.parse_url(self.path)
 
-        # It's an if..else statement
-        if self.path == "/animals":
-            response = get_all_animals()
-        else:
-            response = []
+        if resource == "animals":
+            if id is not None:
+                response = f"{get_single_animal(id)}"
 
-        # This weird code sends a response back to the client
-        self.wfile.write(f"{response}".encode())
+            else:
+                response = f"{get_all_animals()}"
+
+        if resource == "customers":
+            if id is not None:
+                response = f"{get_single_customer(id)}"
+
+            else:
+                response = f"{get_all_customers()}"
+
+        if resource == "employees":
+            if id is not None:
+                response = f"{get_single_employee(id)}"
+
+            else:
+                response = f"{get_all_employees()}"
+
+        if resource == "locations":
+            if id is not None:
+                response = f"{get_single_location(id)}"
+
+            else:
+                response = f"{get_all_locations()}"
+
+        self.wfile.write(response.encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
     def do_POST(self):
-        # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_animal = None
+        new_employee = None
+        new_location = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+
+        if resource == "employees":
+            new_employee = create_employee(post_body)
+        
+        if resource == "locations":
+            new_location = create_location(post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write(f"{new_animal}".encode())
+        self.wfile.write(f"{new_employee}".encode())
+        self.wfile.write(f"{new_location}".encode())
 
 
     # Here's a method on the class that overrides the parent's method.
@@ -54,3 +121,7 @@ def main():
     host = ''
     port = 8088
     HTTPServer((host, port), HandleRequests).serve_forever()
+
+
+if __name__ == "__main__":
+    main()
