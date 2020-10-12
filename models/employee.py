6572c19427690a2b33d7cd3aa0aebaee78575e1d
mswinkels09 <mswinkels09@gmail.com>
2020-10-12 11:47:50 -0500

    integrated data using SQL tables, recalibrated all resources and single resources from kennel.sql

--- models/employee.py
+++ models/employee.py
@@ -3,7 +3,9 @@ class Employee():
     # Class initializer. It has 5 custom parameters, with the
     # special `self` parameter that every method on a class
     # needs as the first parameter.
-    def __init__(self, name, location_id):
+    def __init__(self, unique_id, name, address, location_id):
+        self.id = unique_id
         self.name = name
+        self.address = address
         self.location_id = location_id
 
--- models/location.py
+++ models/location.py
@@ -3,7 +3,8 @@ class Location():
     # Class initializer. It has 5 custom parameters, with the
     # special `self` parameter that every method on a class
     # needs as the first parameter.
-    def __init__(self, name, address):
+    def __init__(self, unique_id, name, address):
+        self.id = unique_id
         self.name = name
         self.address = address
 
